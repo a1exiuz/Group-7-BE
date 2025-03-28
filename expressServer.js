fb8878711673ff95835d178db4bffe2c1a47d14c
a1exiuz <alexis1998ruiz@hotmail.com>
2025-03-28 16:33:19 -0500

    Update expressServer.js

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from "cors";
 import signupRoutes from "./routes/signupRoutes.js";
 import specificRoutes from "./routes/specificRoutes.js";
+import loginRoutes from "./routes/loginRoutes.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use("/api", signupRoutes);
 app.use('/api/specific', specificRoutes);
+app.use("/api", loginRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
