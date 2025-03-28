6a193ee42465ce273b1940b10a902b5bc3d2d090
a1exiuz <alexis1998ruiz@hotmail.com>
2025-03-28 16:42:34 -0500

    Update testIdToken.js

--- testIdToken.js
+++ testIdToken.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3Vr8hFYomcb_r59zmPNN9E4ccEs-w9v4",
-  authDomain: "coursemanagementsystem-8d873.firebaseapp.com",
-  projectId: "coursemanagementsystem-8d873",
+  apiKey: "from firebase",
+  authDomain: "from firebase",
+  projectId: "from firebase",
 };
 
 const app = initializeApp(firebaseConfig);
