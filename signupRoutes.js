fcf29abfff650a79c4e58e2560116daae49e5876
a1exiuz <alexis1998ruiz@hotmail.com>
2025-03-28 16:36:50 -0500

    Update signupRoutes.js

--- signupRoutes.js
+++ signupRoutes.js
@@ -4,10 +4,10 @@ import { signUpUser } from "../services/userSignup.js";
 const router = express.Router();
 
 router.post("/signup", async (req, res) =>{
-    const { email, password, role } =req.body;
+    const { email, password, firstName, lastName, age } =req.body;
 
     try{
-        const result = await signUpUser(email, password, role);
+        const result = await signUpUser(email, password, firstName, lastName, age);
         res.status(201).json({ success: true, user: result});
     }   catch(error) {
         res.status(500).json({ success: false, error: error.message });
